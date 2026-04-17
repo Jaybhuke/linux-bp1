b2723682f41f7ad7791165588a072a8c698ef549
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-17 19:24:50 +0000

    Disk usage added insystem health Monitor

--- health.sh
+++ health.sh
@@ -7,3 +7,7 @@ HOSTNAME=$(hostname)
 
 echo "--------------------------" >> $LOG_FILE
 echo "Health check at [ $DATE ] on [ $HOSTNAME ]" >> $LOG_FILE
+
+
+USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+echo "Disk Usage: $USAGE%" >> $LOG_FILE
