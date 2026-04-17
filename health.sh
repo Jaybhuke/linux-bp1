1cdf233d1614cd2690be41a09021fd065e703a61
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-17 19:51:53 +0000

    CPU Usage added in script

--- health.sh
+++ health.sh
@@ -15,3 +15,9 @@ echo "Disk Usage: $USAGE%" >> $LOG_FILE
 
 MEMORY=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
 echo "Memory Usage: $MEMORY%" >> $LOG_FILE
+
+
+CPU=$(top -b -n 1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+echo "CPU Usage: $CPU%" >> $LOG_FILE
+
+echo "--------------------------" >> $LOG_FILE
