c068bd43e9c0c50b339ccc4e0112694343475724
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-17 19:43:25 +0000

    Memory Usage added in system health check

--- health.sh
+++ health.sh
@@ -11,3 +11,7 @@ echo "Health check at [ $DATE ] on [ $HOSTNAME ]" >> $LOG_FILE
 
 USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
 echo "Disk Usage: $USAGE%" >> $LOG_FILE
+
+
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+echo "Memory Usage: $MEMORY%" >> $LOG_FILE
